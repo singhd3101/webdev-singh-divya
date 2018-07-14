c02748c1229d4baf0993a1f1da2f428aee28c335
Divya Singh <singh.d@husky.neu.edu>
2018-07-14 15:55:13 -0400

    generating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,18 @@ import {TestService} from './services/test.service.client';
 import { LoginComponent } from './components/user/login/login.component';
 import { ProfileComponent } from './components/user/profile/profile.component';
 import { RegisterComponent } from './components/user/register/register.component';
+import { WebsiteNewComponent } from './components/website/website-new/website-new.component';
+import { WebsiteEditComponent } from './components/website/website-edit/website-edit.component';
+import { WebsiteListComponent } from './components/website/website-list/website-list.component';
+import { PageNewComponent } from './components/pages/page-new/page-new.component';
+import { PageEditComponent } from './components/pages/page-edit/page-edit.component';
+import { PageListComponent } from './components/pages/page-list/page-list.component';
+import { WidgetChooserComponent } from './components/widget/widget-chooser/widget-chooser.component';
+import { WidgetEditComponent } from './components/widget/widget-edit/widget-edit.component';
+import { WidgetListComponent } from './components/widget/widget-list/widget-list.component';
+import { WidgetHeaderComponent } from './components/widget/widget-edit/widget-header/widget-header.component';
+import { WidgetImageComponent } from './components/widget/widget-edit/widget-image/widget-image.component';
+import { WidgetYoutubeComponent } from './components/widget/widget-edit/widget-youtube/widget-youtube.component';
 
 @NgModule({
   // Declare components here
@@ -20,7 +32,19 @@ import { RegisterComponent } from './components/user/register/register.component
     TestComponent,
     LoginComponent,
     ProfileComponent,
-    RegisterComponent
+    RegisterComponent,
+    WebsiteNewComponent,
+    WebsiteEditComponent,
+    WebsiteListComponent,
+    PageNewComponent,
+    PageEditComponent,
+    PageListComponent,
+    WidgetChooserComponent,
+    WidgetEditComponent,
+    WidgetListComponent,
+    WidgetHeaderComponent,
+    WidgetImageComponent,
+    WidgetYoutubeComponent
   ],
   imports: [
     BrowserModule,
